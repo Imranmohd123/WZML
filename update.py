36cb321e7b859b4956979bcfeb1ebadcca7cb1bc
Imranmohd123 <116245461+Imranmohd123@users.noreply.github.com>
2022-12-31 16:05:45 +0530

    Update update.py

--- update.py
+++ update.py
@@ -48,8 +48,8 @@ if ospath.exists('.git'):
     srun(["rm", "-rf", ".git"])
 
 update = srun([f"git init -q \
-                 && git config --global user.email doc.adhikari@gmail.com \
-                 && git config --global user.name Karan \
+                 && git config --global user.email doc.kannanram25864@gmail.com \
+                 && git config --global user.name Imranmohd123 \
                  && git add . \
                  && git commit -sm update -q \
                  && git remote add origin {UPSTREAM_REPO} \
